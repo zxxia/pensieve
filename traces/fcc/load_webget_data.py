92e05fcd7d10e9a283ccf2b8b028ed77bcbd3cb3
Zhengxu Xia <kevinxzx95@gmail.com>
2020-10-28 15:28:23 -0500

    Better script for creating fcc simulation trace.

--- traces/fcc/load_webget_data.py
+++ traces/fcc/load_webget_data.py
@@ -1,47 +1,84 @@
+""" Parse fcc webget csv file and output traces for simulation."""
+import argparse
+import csv
+from datetime import datetime
+import os
+
 import numpy as np
-import datetime
 
-
-# FILE_PATH = './201606/curr_webget_2015_06.csv'
-FILE_PATH = './201606/curr_webget_2016_06.csv'
-OUTPUT_PATH = './cooked/'
 NUM_LINES = np.inf
-TIME_ORIGIN = datetime.datetime.utcfromtimestamp(0)
+TIME_INTERVAL = 5  # seconds
+BITS_IN_BYTE = 8  # 8 bits per byte
+MICROSEC_IN_SEC = 1000000.0
 
 
-bw_measurements = {}
+def parse_args():
+    '''
+    Parse arguments from the command line.
+    '''
+    parser = argparse.ArgumentParser("Parse FCC data.")
+    parser.add_argument("--input_file", type=str, required=True,
+                        help='path to a fcc data csv file.')
+    parser.add_argument("--output_dir", type=str, required=True,
+                        help='output directory.')
+
+    return parser.parse_args()
+
+
 def main():
-	line_counter = 0
-	with open(FILE_PATH, 'r') as f:
-		for line in f:
-			parse = line.split(',')
+    args = parse_args()
+    input_file = args.input_file
+    output_dir = args.output_dir
+    os.makedirs(output_dir, exist_ok=True)
+    time_origin = datetime.utcfromtimestamp(0)
+    line_counter = 0
+    bw_measurements = {}
+    unique_targets = set()
+    unique_addresses = set()
+    with open(input_file, 'r') as f:
+        csvreader = csv.reader(f)
+        # refer to
+        # http://data.fcc.gov/download/measuring-broadband-america/2016/data-dictionary-sept2015.xls
+        # for each column name
+        for row in csvreader:
 
-			uid = parse[0]
-			dtime = (datetime.datetime.strptime(parse[1],'%Y-%m-%d %H:%M:%S')
-				- TIME_ORIGIN).total_seconds()
-			target = parse[2]
-			address = parse[3]
-			throughput = parse[6]  # bytes per second
+            uid = row[0]  # unique identifier for an individual unit
+            dtime = (datetime.strptime(row[1], '%Y-%m-%d %H:%M:%S') -
+                     time_origin).total_seconds()  # time test finished in sec
+            target = row[2]  # URL to fetch
+            # IP addres connected to to featch content from initial URL
+            address = row[3]
+            unique_targets.add(target)
+            unique_addresses.add(address)
 
-			k = (uid, target)
-			if k in bw_measurements:
-				bw_measurements[k].append(throughput)
-			else:
-				bw_measurements[k] = [throughput]
+            # Average speed of downloading HTML content and then concurrently
+            # downloading all resources (Units: bytes/sec)
+            throughput = row[6]
 
-			line_counter += 1
-			if line_counter >= NUM_LINES:
-				break
+            k = (uid, target)
+            if k in bw_measurements:
+                bw_measurements[k].append(throughput)
+            else:
+                bw_measurements[k] = [throughput]
 
-	for k in bw_measurements:
-		out_file = 'trace_' + '_'.join(k)
-		out_file = out_file.replace(':', '-')
-		out_file = out_file.replace('/', '-')
-		out_file = OUTPUT_PATH + out_file
-		with open(out_file, 'w') as f:
-			for i in bw_measurements[k]:
-				f.write(str(i) + '\n')
+            line_counter += 1
+            if line_counter >= NUM_LINES:
+                break
+    print(len(unique_targets))
+    print(len(unique_addresses))
+
+    for k in bw_measurements:
+        out_file = 'trace_' + '_'.join(k)
+        out_file = out_file.replace(':', '-')
+        out_file = out_file.replace('/', '-')
+        out_file = os.path.join(output_dir, out_file)
+        with open(out_file, 'w') as f:
+            csvwriter = csv.writer(f)
+            for i, bw in enumerate(bw_measurements[k]):
+                csvwriter.writerow(
+                    [i * TIME_INTERVAL,
+                     float(bw) * BITS_IN_BYTE / MICROSEC_IN_SEC])
+
 
 if __name__ == '__main__':
-	main()
-
+    main()
