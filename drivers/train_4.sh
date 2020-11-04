40cdaf18edf5f63dae2333ff5ae159cd1b2804e5
Lesley Chou <lesleychou339@gmail.com>
2020-11-05 00:00:19 +0900

    run norm-Norway on Uchi server

--- drivers/train_4.sh
+++ drivers/train_4.sh
@@ -7,8 +7,8 @@ NOISE=0
 DURATION=1
 TEST_TRACE_PATH="../data/one-trace"
 
-TRAIN_TRACE_PATH_1="../data/Norway-DR-exp/train-3-noise"
-VAL_TRACE_PATH_1="../data/Norway-DR-exp/val-3-noise"
+TRAIN_TRACE_PATH_1="../data/Norway-DR-exp/train-norm-1-1-noise"
+VAL_TRACE_PATH_1="../data/Norway-DR-exp/val-norm-1-1-noise"
 
 #TRAIN_TRACE_PATH_2="../data/Norway-DR-exp/train-2.2-noise"
 #VAL_TRACE_PATH_2="../data/Norway-DR-exp/val-2.2-noise"
@@ -23,7 +23,7 @@ SIMULATOR_DIR="../sim"
 
 
 
-SUMMARY_DIR_1="../results/Norway-DR-train/multiply-uniform-3-train"
+SUMMARY_DIR_1="../results/Norway-DR-train/multiply-norm-1-1-train"
 python ${SIMULATOR_DIR}/multi_agent.py \
     --train_trace_dir ${TRAIN_TRACE_PATH_1} \
     --val_trace_dir ${VAL_TRACE_PATH_1} \
