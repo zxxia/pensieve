aded34c9072f278586c9a0f18edbf93ed9068307
Lesley Chou <lesleychou339@gmail.com>
2020-11-07 00:20:33 +0900

    new train

--- drivers/run.sh
+++ drivers/run.sh
@@ -4,7 +4,8 @@
 set -e
 
 DURATION=1
-TRACE_PATH="../data/LVC-DR-exp/LVC-test"
+#TRACE_PATH="../data/LVC-DR-exp/LVC-test"
+TRACE_PATH="../data/Norway-DR-exp/val-norm-0.6-0.5-noise"
 # TRACE_PATH="../sim/train_sim_traces"
 # TRACE_PATH="../sim/test_sim_traces"
 # TRACE_PATH="../sim/test_sim_traces"
@@ -21,7 +22,7 @@ LOG_FILES=( '1')
 
 
 
-NN_MODELS=("../results/Norway-DR-train/multiply-norm-1-0.5-train/model_saved/nn_model_ep_35700.ckpt")
+NN_MODELS=("../results/Norway-DR-train/multiply-norm-0.6-0.5-train/model_saved/nn_model_ep_76700.ckpt")
 #NN_MODELS=(
 #"../results/Norway-DR-train/multiply-norm-0-1-train/model_saved/nn_model_ep_31000.ckpt"
 #"../results/Norway-DR-train/multiply-norm-0-1-train/model_saved/nn_model_ep_32200.ckpt"
@@ -37,7 +38,7 @@ NN_MODELS=("../results/Norway-DR-train/multiply-norm-1-0.5-train/model_saved/nn_
 
 
     for RANDOM_SEED in 1; do
-      SUMMARY_DIR="../results/DR-new-test/Norway-norm-1-0.5-LVC-test"
+      SUMMARY_DIR="../results/DR-new-test/Norway-norm-0.6-0.5-Norway"
       #     SUMMARY_DIR="../results/noise_${NOISE}"
       #     SUMMARY_DIR="../results/noise_exp/noise_${NOISE}_train"
 
@@ -61,14 +62,14 @@ NN_MODELS=("../results/Norway-DR-train/multiply-norm-1-0.5-train/model_saved/nn_
 #                 --NUMBER_PICK=253 \
 #                 --duration ${DURATION} &
 
-#            python ${SIMULATOR_DIR}/mpc.py \
-#                 --test_trace_dir ${TRACE_PATH} \
-#                   --summary_dir ${SUMMARY_DIR}/seed_1\
-#                 --random_seed ${RANDOM_SEED}  \
-#                 --ROBUST_NOISE=0 \
-#                 --SAMPLE_LENGTH=0 \
-#                 --NUMBER_PICK=0 \
-#                 --duration ${DURATION}
+            python ${SIMULATOR_DIR}/mpc.py \
+                 --test_trace_dir ${TRACE_PATH} \
+                   --summary_dir ${SUMMARY_DIR}/seed_1\
+                 --random_seed ${RANDOM_SEED}  \
+                 --ROBUST_NOISE=0 \
+                 --SAMPLE_LENGTH=0 \
+                 --NUMBER_PICK=0 \
+                 --duration ${DURATION}
           done
 
     done
--- drivers/train_3.sh
+++ drivers/train_3.sh
@@ -7,8 +7,8 @@ NOISE=0
 DURATION=1
 TEST_TRACE_PATH="../data/one-trace"
 
-TRAIN_TRACE_PATH_1="../data/Norway-DR-exp/train-norm-1-0.5-noise"
-VAL_TRACE_PATH_1="../data/Norway-DR-exp/val-norm-1-0.5-noise"
+TRAIN_TRACE_PATH_1="../data/Norway-DR-exp/train-2.5-noise"
+VAL_TRACE_PATH_1="../data/Norway-DR-exp/val-2.5-noise"
 
 #TRAIN_TRACE_PATH_2="../data/Norway-DR-exp/train-2.2-noise"
 #VAL_TRACE_PATH_2="../data/Norway-DR-exp/val-2.2-noise"
@@ -23,7 +23,7 @@ SIMULATOR_DIR="../sim"
 
 
 
-SUMMARY_DIR_1="../results/Norway-DR-train/multiply-norm-1-0.5-train"
+SUMMARY_DIR_1="../results/Norway-DR-train/multiply-uniform-2.5-train"
 python ${SIMULATOR_DIR}/multi_agent.py \
     --train_trace_dir ${TRAIN_TRACE_PATH_1} \
     --val_trace_dir ${VAL_TRACE_PATH_1} \
