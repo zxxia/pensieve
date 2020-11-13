a533109ef343f1c9d159fc02f05f9f881cf901c6
Lesley Chou <lesleychou339@gmail.com>
2020-11-13 18:55:38 +0900

    T_s train on server

--- drivers/train_2.sh
+++ drivers/train_2.sh
@@ -7,8 +7,8 @@ NOISE=0
 DURATION=1
 TEST_TRACE_PATH="../data/one-trace"
 
-TRAIN_TRACE_PATH_1="../data/synthetic-train/train_max_20"
-VAL_TRACE_PATH_1="../data/synthetic-train/val_max_20"
+TRAIN_TRACE_PATH_1="../data/synthetic_traces/train_Ts_20"
+VAL_TRACE_PATH_1="../data/synthetic_traces/val_Ts_2"
 
 #TRAIN_TRACE_PATH_2="../data/Norway-DR-exp/train-2.2-noise"
 #VAL_TRACE_PATH_2="../data/Norway-DR-exp/val-2.2-noise"
@@ -23,8 +23,9 @@ SIMULATOR_DIR="../sim"
 
 
 
-SUMMARY_DIR_1="../results/synthetic-train/train-20"
+SUMMARY_DIR_1="../results/synthetic-train/train_Ts_20"
 python ${SIMULATOR_DIR}/multi_agent.py \
+    --NUM_AGENTS=8 \
     --train_trace_dir ${TRAIN_TRACE_PATH_1} \
     --val_trace_dir ${VAL_TRACE_PATH_1} \
     --test_trace_dir ${TEST_TRACE_PATH} \
