a2899b1396bb998a25dd8d5cd192b3f0edff243b
Zhengxu Xia <kevinxzx95@gmail.com>
2020-11-13 20:32:06 -0600

    Refactor code to generate synthetic trace.

--- sim/synthetic_traces.py
+++ sim/synthetic_traces.py
@@ -1,10 +1,12 @@
 import argparse
 import csv
-# import os
+import logging
+import os
 import random
-import time as time_module
+import time
 
 import numpy as np
+from sympy import N, Symbol, solve
 
 
 def parse_args():
@@ -12,41 +14,38 @@ def parse_args():
     Parse arguments from the command line.
     '''
     parser = argparse.ArgumentParser("Generate synthetic data.")
-    parser.add_argument("--T_l", type=float, required=True,
+    parser.add_argument("--T_l-range", nargs=2, type=float, required=True,
                         help='control the prob_stay')
-    parser.add_argument("--T_s", type=float, required=True,
+    parser.add_argument("--T_s-range", nargs=2, type=float, required=True,
                         help='control how long to recompute noise')
-    parser.add_argument("--cov", type=float, required=True,
+    parser.add_argument("--cov-range", nargs=2, type=float, required=True,
                         help='coefficient used to compute vairance of a state')
-    parser.add_argument("--duration", type=float, required=True,
+    parser.add_argument("--duration-range", nargs=2, type=float, required=True,
                         help='duration of each synthetic trace in seconds.')
     parser.add_argument("--steps", type=int, default=10,
                         help='number of steps')
-    parser.add_argument("--switch-parameter", type=float, required=True,
-                        help='switch parameter solved by polynomial solver')
-    parser.add_argument("--max-throughput", type=float, default=4.3,
-                        help='upper bound of throughput(Mbps)')
-    parser.add_argument("--min-throughput", type=float, default=0.2,
-                        help='lower bound of throughput(Mbps)')
-    parser.add_argument("--output_file", type=str, required=True,
-                        help='Output file name.')
+    parser.add_argument("--throughput-range", nargs=2, type=float,
+                        default=[0.2, 4.3], help='range of throughput(Mbps)')
+    parser.add_argument("--num-traces", type=int, required=True, help="number"
+                        " of traces to be generated.")
+    parser.add_argument("--output-dir", type=str, required=True,
+                        help='Output file directory.')
 
     return parser.parse_args()
+
+
+def check_args(args):
+    assert args.T_l_range[0] <= args.T_l_range[1]
+    assert args.T_s_range[0] <= args.T_s_range[1]
+    assert args.cov_range[0] <= args.cov_range[1]
+    assert args.duration_range[0] <= args.duration_range[1]
+    assert args.throughput_range[0] <= args.throughput_range[1]
 
 
 def transition(state, variance, prob_stay, bitrate_states_low_var,
                transition_probs):
     # variance_switch_prob, sigma_low, sigma_high,
     transition_prob = random.uniform(0, 1)
-
-    # pick next variance first
-    # variance_switch = random.uniform(0, 1)
-    # next_variance = variance
-    # if (variance_switch < variance_switch_prob):
-    #     if (next_variance == sigma_low):
-    #         next_variance = sigma_high
-    #     else:
-    #         next_variance = sigma_low
 
     if transition_prob < prob_stay:  # stay in current state
         return state
@@ -88,33 +87,21 @@ def transition(state, variance, prob_stay, bitrate_states_low_var,
             return switch_up
 
 
-def main():
-    args = parse_args()
-    T_s = args.T_s
-    T_l = args.T_l
-    cov = args.cov
-    time_length = args.duration
-    output_file = args.output_file
-    output_writer = csv.writer(open(output_file, 'w', 1), delimiter='\t')
+def generate_trace(T_s, T_l, cov, time_length, steps, max_throughput,
+                   min_throughput, switch_parameter, output_file):
     # get bitrate levels (in Mbps)
     bitrate_states_low_var = []
-    curr = args.min_throughput
-    for _ in range(0, args.steps):
+    curr = min_throughput
+    for _ in range(0, steps):
         bitrate_states_low_var.append(curr)
-        curr += ((args.max_throughput - args.min_throughput)/(args.steps - 1))
+        curr += ((max_throughput - min_throughput)/(steps - 1))
 
     # list of transition probabilities
     transition_probs = []
     # assume you can go steps-1 states away (we will normalize this to the
     # actual scenario)
-    for z in range(1, args.steps-1):
-        transition_probs.append(1/(args.switch_parameter**z))
-    # two variance levels
-    # sigma_low = 1.0
-    # sigma_high = 1.0
-
-    # probability of switching variance levels
-    # variance_switch_prob = 0.2
+    for z in range(1, steps-1):
+        transition_probs.append(1/(switch_parameter**z))
 
     # probability to stay in same state
     prob_stay = 1 - 1 / T_l
@@ -123,26 +110,60 @@ def main():
 
     current_state = random.randint(0, len(bitrate_states_low_var)-1)
     current_variance = cov * bitrate_states_low_var[current_state]
-    time = 0
+    ts = 0
     cnt = 0
-    while time < time_length:
-        # prints timestamp (in seconds) and throughput (in Mbits/s)
-        if cnt <= 0:
-            noise = np.random.normal(0, current_variance, 1)[0]
-            cnt = T_s
-        # TODO: the gaussian val is at least 0.1
-        gaus_val = max(0.1, bitrate_states_low_var[current_state] + noise)
-        output_writer.writerow([time, gaus_val, cnt])
-        cnt -= 1
-        next_val = transition(current_state, current_variance, prob_stay,
-                              bitrate_states_low_var, transition_probs)
-        # print(next_vals)
-        if current_state != next_val:
-            cnt = 0
-        current_state = next_val
-        current_variance = cov * bitrate_states_low_var[current_state]
-        time += 1
-    # print('e2e running used {}s'.format(time_module.time()-start))
+    with open(output_file, 'w', 1) as f:
+        output_writer = csv.writer(f, delimiter='\t')
+        while ts < time_length:
+            # prints timestamp (in seconds) and throughput (in Mbits/s)
+            if cnt <= 0:
+                noise = np.random.normal(0, current_variance, 1)[0]
+                cnt = T_s
+            # TODO: the gaussian val is at least 0.1
+            gaus_val = max(0.1, bitrate_states_low_var[current_state] + noise)
+            output_writer.writerow([ts, gaus_val, cnt])
+            cnt -= 1
+            next_val = transition(current_state, current_variance, prob_stay,
+                                  bitrate_states_low_var, transition_probs)
+            if current_state != next_val:
+                cnt = 0
+            current_state = next_val
+            current_variance = cov * bitrate_states_low_var[current_state]
+            ts += 1
+
+
+def main():
+    args = parse_args()
+    check_args(args)
+    os.makedirs(args.output_dir, exist_ok=True)
+    t_start = time.time()
+    eq = -1
+    x = Symbol("x", positive=True)
+    for y in range(1, args.steps-1):
+        eq += (1/x**y)
+    res = solve(eq, x)
+    switch_parameter = N(res[0])
+    print('solving polynomial: {:.3f}s'.format(time.time()-t_start))
+    with open(os.path.join(args.output_dir, "metadata.csv"), 'w', 1) as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(['filename', 'T_s', 'T_l', "cov", 'duration',
+                            'steps', 'max_throughput', 'min_throughput',
+                            'switch_parameter'])
+        for i in range(args.num_traces):
+            output_file = os.path.join(args.output_dir,
+                                       "trace{}.txt".format(i))
+            T_s = random.uniform(*args.T_s_range)
+            T_l = random.uniform(*args.T_l_range)
+            cov = random.uniform(*args.cov_range)
+            duration = random.uniform(*args.duration_range)
+            min_throughput, max_throughput = args.throughput_range
+
+            generate_trace(T_s, T_l, cov, duration, args.steps, max_throughput,
+                           min_throughput, switch_parameter, output_file)
+            csvwriter.writerow([output_file, T_s, T_l, cov, duration,
+                                args.steps, max_throughput, min_throughput,
+                                switch_parameter])
+    print('e2e: {:.3f}s'.format(time.time()-t_start))
 
 
 if __name__ == "__main__":
