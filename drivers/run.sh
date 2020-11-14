179d3224796b0d478ce4ee37247ac373fd9bb9cf
Lesley Chou <lesleychou339@gmail.com>
2020-11-14 13:06:15 +0900

    none

--- drivers/run.sh
+++ drivers/run.sh
@@ -21,7 +21,7 @@ LOG_FILES=( '1')
 
 
 
-NN_MODELS="../results/synthetic-train/train-100/model_saved/nn_model_ep_24400.ckpt"
+NN_MODELS="../results/synthetic-train/train_Ts_100/model_saved/nn_model_ep_27900.ckpt"
 #NN_MODELS=(
 #"../results/Norway-DR-train/multiply-norm-0-1-train/model_saved/nn_model_ep_31000.ckpt"
 #"../results/Norway-DR-train/multiply-norm-0-1-train/model_saved/nn_model_ep_32200.ckpt"
@@ -29,9 +29,11 @@ NN_MODELS="../results/synthetic-train/train-100/model_saved/nn_model_ep_24400.ck
 #RANDOM_SEED=41
 
 
-for i_folder in 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100; do
-        TRACE_PATH="../data/synthetic-train/test_large_range/${i_folder}"
-        SUMMARY_DIR="../results/synthetic-test/large-range-test-real/train-100/test-on-${i_folder}"
+#for i_folder in 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100; do
+for i_folder in 0 10 20 30 40 50 60 70 80 90 100; do
+
+        TRACE_PATH="../data/synthetic_train/train_Ts_test/${i_folder}/"
+        SUMMARY_DIR="../results/synthetic_test/train_Ts/train_Ts_100/test-on-${i_folder}"
 
         #for ((i=0;i<${#NN_MODELS[@]};++i)); do
             python ${SIMULATOR_DIR}/rl_test.py \
