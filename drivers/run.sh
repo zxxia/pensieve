311b10033dd3b285335fdb07721320f26152ddac
Lesley Chou <lesleychou339@gmail.com>
2020-11-16 23:06:06 +0900

    none

--- drivers/run.sh
+++ drivers/run.sh
@@ -33,20 +33,20 @@ NN_MODELS="../results/synthetic-train/Ts-train/train_Ts_50/model_saved/nn_model_
 #TRACE_PATH="../data/synthetic-train/train_maxBW_60/"
 #SUMMARY_DIR="../results/synthetic-test/test-on-train_maxBW_60/train-20/"
 
-for i_folder in 2 10 20 30 40 50 60 70 80 90 100; do
-        TRACE_PATH="../data/synthetic_test/Ts_train_test/${i_folder}"
-        SUMMARY_DIR="../results/synthetic-test/Ts_train/train_Ts_50/test-on-${i_folder}"
+for i_folder in 5 10 20 30 40 50 60 70 80 90 100; do
+        TRACE_PATH="../data/synthetic-test/test_more_bitrate/test_on_${i_folder}"
+        SUMMARY_DIR="../results/synthetic-test/more-bitrate-tmp/mpc/test-on-${i_folder}"
 
         #for ((i=0;i<${#NN_MODELS[@]};++i)); do
-            python ${SIMULATOR_DIR}/rl_test.py \
-                   --test_trace_dir ${TRACE_PATH} \
-                   --summary_dir ${SUMMARY_DIR}/seed_1\
-                   --model_path ${NN_MODELS} \
-                   --random_seed=1 \
-                   --ROBUST_NOISE=0 \
-                   --SAMPLE_LENGTH=0 \
-                   --NUMBER_PICK=0 \
-                   --duration ${DURATION} &
+#            python ${SIMULATOR_DIR}/rl_test.py \
+#                   --test_trace_dir ${TRACE_PATH} \
+#                   --summary_dir ${SUMMARY_DIR}/seed_1\
+#                   --model_path ${NN_MODELS} \
+#                   --random_seed=1 \
+#                   --ROBUST_NOISE=0 \
+#                   --SAMPLE_LENGTH=0 \
+#                   --NUMBER_PICK=0 \
+#                   --duration ${DURATION} &
 
 #           python ${SIMULATOR_DIR}/bb.py \
 #                 --test_trace_dir ${TRACE_PATH} \
@@ -57,14 +57,14 @@ for i_folder in 2 10 20 30 40 50 60 70 80 90 100; do
 #                 --NUMBER_PICK=253 \
 #                 --duration ${DURATION} &
 #
-#            python ${SIMULATOR_DIR}/mpc.py \
-#                 --test_trace_dir ${TRACE_PATH} \
-#                   --summary_dir ${SUMMARY_DIR}/seed_1\
-#                 --random_seed=1  \
-#                 --ROBUST_NOISE=0 \
-#                 --SAMPLE_LENGTH=0 \
-#                 --NUMBER_PICK=0 \
-#                 --duration ${DURATION}
+            python ${SIMULATOR_DIR}/mpc.py \
+                 --test_trace_dir ${TRACE_PATH} \
+                   --summary_dir ${SUMMARY_DIR}/seed_1\
+                 --random_seed=1  \
+                 --ROBUST_NOISE=0 \
+                 --SAMPLE_LENGTH=0 \
+                 --NUMBER_PICK=0 \
+                 --duration ${DURATION}
 ##          #done
 
 done
