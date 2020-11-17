ebf2d25225d6a1ca2965e6a938457ca612f08cc3
Zhengxu Xia <kevinxzx95@gmail.com>
2020-11-17 10:54:33 -0600

    Save driver scripts

--- drivers/run.sh
+++ drivers/run.sh
@@ -10,14 +10,17 @@ DURATION=1
 # TRACE_PATH="../sim/test_sim_traces"
 # TRACE_PATH="../sim/test_sim_traces"
 # TRACE_PATH="../data/test"
+TRACE_PATH="../data/synthetic_traces/trace_to_test"
 # TRACE_PATH="../data/train"
 # TRACE_PATH="../data/Lesley_more_traces_for_pensieve/cooked_test_traces"
 # TRACE_PATH="../data/Lesley_more_traces_for_pensieve/cooked_traces"
 # TRACE_PATH="../data/exponential_traces/test"
 # TRACE_PATH="../data/step_traces/test"
 # TRACE_PATH="../data/step_traces_period20/test"
-TRACE_PATH="../data/step_traces_period50/test"
+# TRACE_PATH="../data/step_traces_period50/test"
 # TRACE_PATH="../data/step_traces_period40_changing_peak/test"
+# TRACE_PATH="../data/201608_train"
+# TRACE_PATH="../data/201608_test"
 # SUMMARY_DIR="../results/duration_exp/duration_quarter"
 # NN_MODELS=( "../sim/results_noise0.5/nn_model_ep_5300.ckpt" )
 SIMULATOR_DIR="../sim"
@@ -34,11 +37,10 @@ LOG_FILES=( '0' '1' '2' '3' )
 #              # "../results/bug_fix/results_noise4/nn_model_ep_20500.ckpt" )
 
 # extropy fixed models
-NN_MODELS=(
-             "../results/entropy_weight_exp/results_noise0/nn_model_ep_83400.ckpt"
-             "../results/bug_fix/results_noise1/nn_model_ep_28100.ckpt"
-             "../results/entropy_weight_exp/results_noise2/nn_model_ep_64200.ckpt"
-             "../results/bug_fix/results_noise3/nn_model_ep_95100.ckpt" )
+NN_MODELS=( "../results/entropy_weight_exp/results_noise0/nn_model_ep_83400.ckpt")
+             # "../results/bug_fix/results_noise1/nn_model_ep_28100.ckpt"
+             # "../results/entropy_weight_exp/results_noise2/nn_model_ep_64200.ckpt"
+             # "../results/bug_fix/results_noise3/nn_model_ep_95100.ckpt" )
              # "../results/bug_fix/results_noise4/nn_model_ep_20500.ckpt" )
 
 # exponential_traces
@@ -48,7 +50,20 @@ NN_MODELS=(
 # NN_MODELS=( "../results/step_traces/results_noise_0_duration_1/nn_model_ep_6200.ckpt" )
 # NN_MODELS=( "../results/step_traces_period20/results_noise_0_duration_1/nn_model_ep_25000.ckpt" )
 # NN_MODELS=( "../results/step_traces_period40_changing_peak/results_noise_0_duration_1/nn_model_ep_45900.ckpt" )
-NN_MODELS=( "../results/step_traces_period50/results_noise_0_duration_1/nn_model_ep_18600.ckpt" )
+# NN_MODELS=( "../results/step_traces_period50/results_noise_0_duration_1/nn_model_ep_18600.ckpt" )
+# NN_MODELS=( "../results/1000_fcc201608_traces/noise_0/nn_model_ep_82500.ckpt" )
+# NN_MODELS=( "../results/results/results_noise_0/nn_model_ep_19500.ckpt" )
+# NN_MODELS=( "../results/results_original/results_noise_0/nn_model_ep_17800.ckpt" )
+# NN_MODELS=( "../results/results_original_change_pred/results_noise_0/nn_model_ep_108500.ckpt" )
+# NN_MODELS=( "../results/repeat_entropy_weight_exp1/results_noise_0/nn_model_ep_164500.ckpt" )
+# NN_MODELS=( "../results/remove_bad_traces/results_noise_0/nn_model_ep_58200.ckpt" )
+# NN_MODELS=( "../results/synthetic_traces_T_l_1_2_exp/results_noise_0/nn_model_ep_22400.ckpt" )
+# NN_MODELS=( "../results/synthetic_traces_T_l_1_6_exp/results_noise_0/nn_model_ep_24300.ckpt" )
+# NN_MODELS=( "../results/synthetic_traces_T_l_1_10_exp/results_noise_0/nn_model_ep_21900.ckpt" )
+NN_MODELS=( "../results/synthetic_traces_duration_10_30_exp/results_noise_0/nn_model_ep_39000.ckpt" )
+NN_MODELS=( "../results/synthetic_traces_duration_10_100_exp/results_noise_0/nn_model_ep_3800.ckpt" )
+NN_MODELS=( "../results/synthetic_traces_duration_10_300_exp/results_noise_0/nn_model_ep_4900.ckpt" )
+NN_MODELS=( "../results/synthetic_traces_duration_10_500_exp/results_noise_0/nn_model_ep_4800.ckpt" )
 for NOISE in 0 ; do
     # SUMMARY_DIR="../results/noise_exp/noise_${NOISE}"
     # SUMMARY_DIR="../results/tmp/noise_${NOISE}"
@@ -62,7 +77,20 @@ for NOISE in 0 ; do
     # SUMMARY_DIR="../results/step_traces/noise_${NOISE}"
     # SUMMARY_DIR="../results/step_traces_period20/noise_${NOISE}"
     # SUMMARY_DIR="../results/step_traces_period40_changing_peak/noise_${NOISE}"
-    SUMMARY_DIR="../results/step_traces_period50/noise_${NOISE}"
+    # SUMMARY_DIR="../results/step_traces_period50/noise_${NOISE}"
+    # SUMMARY_DIR="../results/1000_fcc201608_traces/noise_${NOISE}_test_original"
+    # SUMMARY_DIR="../results/results/noise_${NOISE}"
+    # SUMMARY_DIR="../results/results_original/noise_${NOISE}"
+    # SUMMARY_DIR="../results/results_original_change_pred/noise_${NOISE}"
+    # SUMMARY_DIR="../results/repeat_entropy_weight_exp1/noise_${NOISE}"
+    # SUMMARY_DIR="../results/remove_bad_traces/noise_${NOISE}"
+    # SUMMARY_DIR="../results/synthetic_traces_T_l_1_2_exp/noise_${NOISE}"
+    # SUMMARY_DIR="../results/synthetic_traces_T_l_1_6_exp/noise_${NOISE}"
+    # SUMMARY_DIR="../results/synthetic_traces_T_l_1_10_exp/noise_${NOISE}"
+    SUMMARY_DIR="../results/synthetic_traces_duration_10_30_exp/noise_${NOISE}"
+    SUMMARY_DIR="../results/synthetic_traces_duration_10_100_exp/noise_${NOISE}"
+    SUMMARY_DIR="../results/synthetic_traces_duration_10_300_exp/noise_${NOISE}"
+    SUMMARY_DIR="../results/synthetic_traces_duration_10_500_exp/noise_${NOISE}"
     # python ${SIMULATOR_DIR}/bb.py \
     #     --test_trace_dir ${TRACE_PATH} \
     #     --summary_dir ${SUMMARY_DIR}/bb\
@@ -73,6 +101,7 @@ for NOISE in 0 ; do
     #     --summary_dir ${SUMMARY_DIR}/mpc\
     #     --noise ${NOISE} \
     #     --duration ${DURATION} &
+    # cp -r ../results/entropy_weight_exp/noise_0/mpc ${SUMMARY_DIR}/
 
     for ((i=0;i<${#NN_MODELS[@]};++i)); do
         python ${SIMULATOR_DIR}/rl_test.py \
@@ -80,7 +109,7 @@ for NOISE in 0 ; do
             --summary_dir ${SUMMARY_DIR}/sim_rl_train_noise${LOG_FILES[i]}\
             --model_path ${NN_MODELS[i]} \
             --noise ${NOISE} \
-            --duration ${DURATION} &
+            --duration ${DURATION}
     done
     # python ${SIMULATOR_DIR}/rl_test.py \
     #     --test_trace_dir ${TRACE_PATH} \
--- drivers/train.sh
+++ drivers/train.sh
@@ -5,9 +5,15 @@ set -e
 
 NOISE=0
 DURATION=1
-TRAIN_TRACE_PATH="../data/train"
-VAL_TRACE_PATH="../data/val"
-TEST_TRACE_PATH="../data/test"
+# TRAIN_TRACE_PATH="../data/train"
+# VAL_TRACE_PATH="../data/val"
+# TEST_TRACE_PATH="../data/test"
+# TRAIN_TRACE_PATH="../data/synthetic_traces/train"
+# VAL_TRACE_PATH="../data/synthetic_traces/val"
+# TEST_TRACE_PATH="../data/synthetic_traces/test"
+# TRAIN_TRACE_PATH="../data/201608_train"
+# VAL_TRACE_PATH="../data/201608_val"
+# TEST_TRACE_PATH="../data/201608_test"
 # TRAIN_TRACE_PATH="../data/exponential_traces/train"
 # VAL_TRACE_PATH="../data/exponential_traces/val"
 # TEST_TRACE_PATH="../data/exponential_traces/test"
@@ -28,6 +34,9 @@ TEST_TRACE_PATH="../data/test"
 # TEST_TRACE_PATH="../data/constant_trace/test"
 SIMULATOR_DIR="../sim"
 
+TRAIN_TRACE_PATH="../data/synthetic_traces/train_T_l_1_10"
+VAL_TRACE_PATH="../data/synthetic_traces/val_T_l_1_10"
+TEST_TRACE_PATH="../data/synthetic_traces/test_T_l_1_10"
 
 LOG_FILES=( '0' '1' '2' '3' )
 
@@ -41,12 +50,28 @@ for NOISE in 0 ; do
     # SUMMARY_DIR="../results/constant_trace/results_noise_${NOISE}_duration_${DURATION}"
     # SUMMARY_DIR="../results/eval_train_e2e/results_noise_${NOISE}_duration_${DURATION}"
     # SUMMARY_DIR="../results/eval_train_e2e_new/results_noise_${NOISE}_duration_${DURATION}"
-    SUMMARY_DIR="../results/remove_bad_traces/noise_${NOISE}"
+    # SUMMARY_DIR="../results/remove_bad_traces/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/1000_fcc201608_traces/noise_${NOISE}"
+    # SUMMARY_DIR="../results/1000_fcc201608_traces/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/1000_fcc201608_traces/results_noise_${NOISE}_changing_lr"
+    # SUMMARY_DIR="../results/changing_lr/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/repeat_entropy_weight_exp/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/results_compare/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/results_train_on_synthetic/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/repeat_entropy_weight_exp1/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/entropy_weight_paper_exp/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/entropy_weight_paper_exp2/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/results_original/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/results_original_change_pred/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/synthetic_traces_T_l_1_2_exp/results_noise_${NOISE}"
+    # SUMMARY_DIR="../results/synthetic_traces_T_l_1_6_exp/results_noise_${NOISE}"
+    SUMMARY_DIR="../results/synthetic_traces_T_l_1_10_exp/results_noise_${NOISE}"
     python ${SIMULATOR_DIR}/multi_agent.py \
         --train_trace_dir ${TRAIN_TRACE_PATH} \
         --val_trace_dir ${VAL_TRACE_PATH} \
         --test_trace_dir ${TEST_TRACE_PATH} \
         --summary_dir ${SUMMARY_DIR} \
         --noise ${NOISE} \
-        --duration ${DURATION}
+        --duration ${DURATION} \
+        --NUM_AGENTS 16
 done
